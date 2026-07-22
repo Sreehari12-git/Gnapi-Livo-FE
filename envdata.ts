cca63e5ba6750b5a6fad7f4f45cb0f6e3acd8cdb
Sreehari K P <Sreehari.KP@gnapi.tech>
2026-07-22 17:04:30 +0530

    Give it for production build

--- envdata.ts
+++ envdata.ts
@@ -1,3 +1,3 @@
-export const EXPO_PUBLIC_BASE_URL = "https://waggle-survive-babble.ngrok-free.dev"
+export const EXPO_PUBLIC_BASE_URL = "https://gnapi-livo-be.onrender.com"
 // Replace with the LAN IP of the machine running ai-director (uvicorn main:app --host 0.0.0.0 --port 8000)
 export const AI_DIRECTOR_URL = "https://immune-notre-consideration-starting.trycloudflare.com"
